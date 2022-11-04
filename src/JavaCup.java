9b0c8c67d98eede727bd56ee40b73f06c63b1cb7
rerfan24 <rezaerfan26@gmail.com>
2022-11-04 19:05:35 +0330

    optimizing JavaCup

--- src/JavaCup.java
+++ src/JavaCup.java
@@ -11,19 +11,19 @@ public class JavaCup {
         int j = scanner.nextInt();
         System.out.println("Press number3: ");
         int k = scanner.nextInt();
-        temp();
-        eval(i, j, k);
+//        temp();
+        eval(i*i, j*j, k*k);
     }
     public static void eval(int i, int j, int k)
     {
-        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k)
+        if (i + j == k || i == j + k || j == i + k)
         {
             System.out.println("YES");
         }
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
+        ArrayList<Integer> a = new ArrayList<>();
         for (int i = 0; i < 10000; i++)
         {
             for (int j = 0; j < 20000; j++) {
